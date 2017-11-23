bead85d809b1b8cdb1167e32cc6340277856b2f6
omarbee <32301196+omarbee@users.noreply.github.com>
2017-11-23 16:36:07 +0100

    Update index.php
    pdo

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
-  echo"lal";
-
-
+try
+{
+    // On se connecte à MySQL
+    $pdo = new PDO('pgsql:ec2-46-137-174-67.eu-west-1.compute.amazonaws.com;dbname=d6ngsqf1ledqck;', 'znpcoimcuailgi', 'a1aff9d7e4d6481a2071aefde60dadae5ab6e83748362e34e418a6e782bc4d32);
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+$query = 'INSERT INTO categories (nom, description) VALUES (?, ?);';
+$prep = $pdo->prepare($query);
+ 
+$prep->bindValue(1, 'bertand', PDO::PARAM_STR);
+$prep->bindValue(2, 'ceci est un test pour desc', PDO::PARAM_STR);
+$prep->execute();
+$resultat = $pdo->query('SELECT * FROM categories');
+while ($donnees = $resultat->fetch())
+{
+  echo '<br/>';
+  echo $donnees['nom'];
+  echo ' : ';
+  echo $donnees['description'];
+}
 ?>
